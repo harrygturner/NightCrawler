f5115a68ecfde8a79254e368a7ee747593c94fb8
Claudia Fox <claudiafox95@icloud.com>
2019-02-12 12:03:37 +0000

    Added full-size Google Map centralised on London to index.html

---
+++ frontend-notes/src/index.js
@@ -0,0 +1,7 @@
+function initMap() {
+  const latlng = new google.maps.LatLng(51.507734, -0.127888)
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: latlng
+  });
+}
