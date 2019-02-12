9fd65fde74a736f2ea63f5acf0929144acf15432
Claudia Fox <claudiafox95@icloud.com>
2019-02-12 15:43:59 +0000

    added geocode feature and option to zoom into buildings

--- frontend-notes/src/index.js
+++ frontend-notes/src/index.js
@@ -1,11 +1,39 @@
-function initMap() {
-  const latlng = new google.maps.LatLng(51.507734, -0.127888)
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: latlng
-  });
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhdWRpZm94IiwiYSI6ImNqczFud2tiNzBlbTI0M2t2aGpuMzBqb2QifQ.xc_ZOhqTlgjd3sIoLBrS9Q';
+let map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+center: [-0.127888, 51.507734], // starting position [lng, lat]
+zoom: 8 // starting zoom
+});
 
-// function geoLocationMap() {
-//   const
-// }
+map.on('load', function() {
+map.setPaintProperty('building', 'fill-color', [
+"interpolate",
+["exponential", 0.5],
+["zoom"],
+15,
+"#e2714b",
+22,
+"#eee695"
+]);
+
+map.setPaintProperty('building', 'fill-opacity', [
+"interpolate",
+["exponential", 0.5],
+["zoom"],
+15,
+0,
+22,
+1
+]);
+});
+
+document.getElementById('zoom').addEventListener('click', function () {
+map.zoomTo(17.5, {duration: 9000});
+});
+
+const geocoder = new MapboxGeocoder({
+accessToken: mapboxgl.accessToken
+});
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
