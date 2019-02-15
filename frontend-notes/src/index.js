35c89df3a52faba6a41202b7c22f8f248b63b3cc
github username <github email address>
2019-02-15 12:07:19 +0000

    website kind of working

--- frontend-notes/src/index.js
+++ frontend-notes/src/index.js
@@ -15,9 +15,91 @@ const state = {
   geojsonIcons: {},
   markers: [],
   selectedMarker: null,
-  unselectedMarkers: [],
   restaurants: [],
-  bars: []
+  restaurantMarkers: [],
+  restaurantSelected: null,
+  selectedRestaurantMarker: null,
+  restaurantAdded: false,
+  bars: [],
+  barMarkers: [],
+  barSelected: null,
+  selectedBarMarker: null,
+  barAdded: false,
+  restSearchedFor: false,
+  barSearchedFor: false
+}
+
+// ---------------------- login & sign up ---------------------------
+// event listeners for signing up and logging into the app
+function signingInEventListener() {
+  document.addEventListener('click', event => {
+    if(event.target.className === 'btn btn-info signup-btn'){
+      renderSignUpForm();
+    } else if(event.target.className === 'btn btn-info login-btn') {
+      renderLogInForm();
+    }
+  })
+}
+
+const rearrangeTitleBox = () => {
+  signInEl.style.width = '300px'
+  signInEl.style.marginLeft = '-150px'
+}
+
+const signInEl = document.querySelector('#title-box')
+// render sign up form
+function renderSignUpForm() {
+  rearrangeTitleBox()
+  signInEl.style.height = '450px'
+  signInEl.style.marginBottom = '-225px'
+  signInEl.innerHTML = `
+    <h2 style='margin: 20px;'>NightCrawler</h2>
+    <form>
+      First name:<br>
+      <input type="text" name="firstname" style="margin-bottom: 20px;"><br>
+      Last name:<br>
+      <input type="text" name="lastname" style="margin-bottom: 20px;"><br>
+      Username:<br>
+      <input type="text" name="username" style="margin-bottom: 20px;"><br>
+      Password:<br>
+      <input type="password" name="username" style="margin-bottom: 20px;"><br>
+      <input type="submit" value="Sign Up" class='btn btn-info'>
+    </form>
+  `
+  const subBtn = signInEl.querySelector('input[type=submit]');
+  subBtn.addEventListener('click', registersAndWelcomeUser);
+}
+
+// registers and welcomes the user to the app
+function registersAndWelcomeUser() {
+  const form = signInEl.querySelector('form');
+  const fullname = form.firstname.value + ' ' + form.lastname.value;
+  signInEl.style.width = '550px'
+  signInEl.style.marginLeft = '-275px'
+  signInEl.style.height = '240px'
+  signInEl.style.marginBottom = '-120px'
+  signInEl.innerHTML = `
+      <h2 style='margin: 20px;'>NightCrawler</h2>
+      <p>Hello ${fullname}, let's start planning your night. If you'd like to search for events around your current location please click on the target icon, or if you want to search a location of your choice just type it in the search bar.</p>
+  `
+  renderGeolocaterAndSearchBarOnMap();
+}
+
+// render log in form
+function renderLogInForm() {
+  rearrangeTitleBox();
+  signInEl.style.height = '300px'
+  signInEl.style.marginBottom = '-150px'
+  signInEl.innerHTML = `
+    <h2 style='margin: 20px;'>NightCrawler</h2>
+    <form>
+      Username:<br>
+      <input type="text" name="lastname" style="margin-bottom: 20px;"><br>
+      Password:<br>
+      <input type="password" name="username" style="margin-bottom: 20px;"><br>
+      <input type="submit" value="Log In" class='btn btn-info'>
+    </form>
+  `
 }
 
 //------------------------- Map Box API -----------------------------
@@ -69,6 +151,11 @@ function formatDate(date) {
 function todayDate() {
   const today = new Date();
   return formatDate(today);
+}
+
+function timeNow() {
+  const today = new Date();
+  return now = today.getHours() + ":" + today.getMinutes();
 }
 
 // ---------------------- Truncate word limit --------------------------
@@ -147,22 +234,22 @@ function renderMarkers() {
     const markerEl = document.createElement('div');
     switch(marker.properties.category){
       case 'community':
-        markerEl.className = 'marker community';
+        markerEl.className = 'community icon';
         break;
       case 'concerts':
-        markerEl.className = 'marker concert';
+        markerEl.className = 'concert icon';
         break;
       case 'performing-arts':
-        markerEl.className = 'marker theatre';
+        markerEl.className = 'theatre icon';
         break;
       case 'conferences':
-        markerEl.className = 'marker conference';
+        markerEl.className = 'conference icon';
         break;
       case 'sports':
-        markerEl.className = 'marker sport';
+        markerEl.className = 'sport icon';
         break;
       default:
-        markerEl.className = 'marker';
+        markerEl.className = 'icon';
     }
     markerEl.dataset.id = marker.properties.id;
     const mapMarker = new mapboxgl.Marker(markerEl)
@@ -170,7 +257,6 @@ function renderMarkers() {
       .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
         .setHTML(`
           <h6>${marker.properties.title}</h6>
-          <button type='button' class='rest-btn hide'>Find Restaurants Nearby</button>
         `))
       .addTo(map);
     state.markers.push(mapMarker);
@@ -180,7 +266,7 @@ function renderMarkers() {
 // add event listener's to markers to identify the event clicked
 function addEventListenerToMarkers(){
   document.addEventListener('click', event => {
-    if(event.target.className.includes('marker')) {
+    if(event.target.className.includes('icon')) {
       const eventId = event.target.dataset.id;
       state.selectedEvent = state.events.filter(event => event.id === eventId )[0];
       state.selectedMarker = state.markers.filter(marker => eventId === marker._element.dataset.id)[0];
@@ -217,7 +303,8 @@ function addUserToSideBar() {
   usernameEl = document.createElement('div');
   usernameEl.className = 'user-info';
   usernameEl.innerHTML = `
-    <h5>Harry Turner</h5>
+    <h5 style='margin-top: 20px;'>Harry Turner</h5>
+    <button class='btn btn-light save-btn hide' style='margin-top: 10px;'>Save My Night</button>
   `
   sidebar.append(usernameEl);
 }
@@ -271,7 +358,7 @@ function addAddBtn() {
   addBtn.className = 'btn btn-light add-btn'
   addBtn.innerText = 'Add to Night'
   addBtn.addEventListener('click', () => {
-    document.querySelector('.rest-btn').classList.remove('hide');
+    addEventToSchedule();
   })
   sidebar.append(addBtn)
 }
@@ -283,100 +370,152 @@ function addAllToSideBar() {
   addAddBtn()
 }
 
-// ---------------------- map stuff -------------------------------
+// when add event button is clicked will add the event to users schedule
+function addEventToSchedule() {
+  const eventInfo = document.querySelector('.event-info');
+  document.querySelector('.save-btn').classList.remove('hide');
+  document.querySelector('.add-btn').remove();
+  const ev = state.selectedEvent;
+  eventInfo.innerHTML = ''
+  eventInfo.className = 'side-schedule'
+  eventInfo.innerHTML = `
+  	<h6 style='margin-left: 8px; padding-top: 20px;'>Schedule</h6>
+    <p style='margin-left: 8px; font-size: 13px'>Current Time: ${timeNow()}</p>
+  	<hr style='width: 90%;'>
+  	<div class='today-events'>
+  		<h6>${ev.title}</h6>
+  		<p>${ev.start.split(/T|:00Z/)[1]}</p>
+  	</div>
+    <hr style='width: 90%;'>
+  `
+  const btns = document.createElement('div');
+  btns.className = 'rest-bar-btns'
+  btns.innerHTML = `
+    <button class='btn btn-light rest-btn'>Find a Restaurant</button><br>
+    <button class='btn btn-light bar-btn'>Find a Bar</button>
+  `
+  sidebar.append(btns);
+}
 
-const geocoder = new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  zoom: 14.5
-});
-
-const findLocation = map.addControl(geocoder);
-
-// Here's the code for locating the user, need to have event listener for if either this or the findLocation is clicked. And then to find events based on that
-const tracker = new mapboxgl.GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true
-  },
-  trackUserLocation: true
-});
-
-const userLocation = map.addControl(tracker);
-
-
-// After the map style has loaded on the page, add a source layer and default
-// styling for a single point.
-  map.on('load', function() {
-    map.addSource('single-point', {
-      "type": "geojson",
-      "data": {
-      "type": "FeatureCollection",
-      "features": []
-    }
+// -------------------------- map stuff -------------------------------
+// function renderGeolocaterAndSearchBarOnMap() {
+  const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    zoom: 14.5
   });
 
-  map.addLayer({
-    "id": "point",
-    "source": "single-point",
-    "type": "circle",
-    "paint": {
-      "circle-radius": 10,
-      "circle-color": "#007cbf"
-    }
+  const findLocation = map.addControl(geocoder);
+
+  // Here's the code for locating the user, need to have event listener for if either this or the findLocation is clicked. And then to find events based on that
+  const tracker = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    trackUserLocation: true
   });
 
+  const userLocation = map.addControl(tracker);
 
-// Listen for the `result` event from the MapboxGeocoder that is triggered when a user
-// makes a selection and add a symbol that matches the result.
-  geocoder.on('result', function(ev) {
-    if(!state.search){
-      state.search = true;
-    } else {
-      removeMarkers(state.markers)
-    }
-    map.getSource('single-point').setData(ev.result.geometry);
-    state.currentLocation  = geocoder._map._easeOptions.center;
-    getEventsFromUserLocation(2);
-    addEventListenerToMarkers();
-  });
 
-  tracker.on('geolocate', ev => {
-    if(!state.tracker) {
-      state.search = true;
-      state.tracker = true;
-      const userLoc = [ ev.coords.longitude, ev.coords.latitude ]
-      state.currentLocation = userLoc;
+  // After the map style has loaded on the page, add a source layer and default
+  // styling for a single point.
+    map.on('load', function() {
+
+
+
+      map.addSource('single-point', {
+        "type": "geojson",
+        "data": {
+        "type": "FeatureCollection",
+        "features": []
+      }
+    });
+
+    map.addLayer({
+      "id": "point",
+      "source": "single-point",
+      "type": "circle",
+      "paint": {
+        "circle-radius": 10,
+        "circle-color": "#007cbf"
+      }
+    });
+
+
+  // Listen for the `result` event from the MapboxGeocoder that is triggered when a user
+  // makes a selection and add a symbol that matches the result.
+    geocoder.on('result', function(ev) {
+      if(!state.search){
+        state.search = true;
+      } else {
+        removeMarkers(state.markers)
+      }
+      signInEl.remove();
+      map.getSource('single-point').setData(ev.result.geometry);
+      state.currentLocation  = geocoder._map._easeOptions.center;
       getEventsFromUserLocation(2);
       addEventListenerToMarkers();
-      restaurantBtnListener();
-    }
-  })
-});
+    });
+
+    tracker.on('geolocate', ev => {
+      if(!state.tracker) {
+        signInEl.remove();
+        state.search = true;
+        state.tracker = true;
+        const userLoc = [ ev.coords.longitude, ev.coords.latitude ]
+        state.currentLocation = userLoc;
+        getEventsFromUserLocation(2);
+        addEventListenerToMarkers();
+        restaurantBtnListener();
+      }
+    })
+    eventListenerForAddRestToSchedule();
+  });
+// }
 
 // ---------------------------- restaurants -----------------------------------
-// add event listener to find restaurant button
+// add event listener to find restaurant and bar button
 function restaurantBtnListener() {
   document.addEventListener('click', event => {
     if(event.target.className.includes('rest-btn')) {
+      state.restSearchedFor = true;
       removeUnselectedMarkers();
       getNearbyRestaurants();
+      if(state.barSearchedFor){
+        removeUnselectedBarMarkers();
+        state.bars = [];
+        state.barMarkers = [];
+        state.barSearchedFor = false
+      }
+    } else if (event.target.className.includes('bar-btn')) {
+      state.barSearchedFor = true;
+      removeUnselectedMarkers();
+      getNearbyBars();
+      if(state.restSearchedFor){
+        removeUnselectedRestaurantMarkers();
+        state.restaurantMarkers = [];
+        state.restSearchedFor = false
+      }
     }
   })
 }
 
 const getNearbyRestaurants = () => {
-  const service = new google.maps.places.PlacesService(document.createElement('div'))
-  const markerLatLng = new google.maps.LatLng({lat: state.selectedEvent.location[1], lng: state.selectedEvent.location[0]})
-  const request = {
-    location: markerLatLng,
-    radius: '500',
-    type: ['restaurant']
-  };
-  state.restaurants = [];
-  let i = 0;
-  service.nearbySearch(request, (places) => {
-    places.forEach((place, i) => renderPlace(place, i))
-    renderRestaurantMarkers();
-  })
+  if(!state.restaurantAdded){
+    const service = new google.maps.places.PlacesService(document.createElement('div'))
+    const markerLatLng = new google.maps.LatLng({lat: state.selectedEvent.location[1], lng: state.selectedEvent.location[0]})
+    const request = {
+      location: markerLatLng,
+      radius: '500',
+      type: ['restaurant']
+    };
+    state.restaurants = [];
+    let i = 0;
+    service.nearbySearch(request, (places) => {
+      places.forEach((place, i) => renderPlace(place, i))
+      renderRestaurantMarkers();
+    })
+  }
 };
 
 const renderPlace = (place, i) => {
@@ -391,7 +530,7 @@ const renderPlace = (place, i) => {
     coordinates: placeCoordinates,
     totalReviews: placeTotalReviews,
     rating: placeRating,
-    priveLevel: placePriceLevel
+    priceLevel: placePriceLevel
   }
   state.restaurants.push(newRestaurant);
   ++i;
@@ -399,7 +538,6 @@ const renderPlace = (place, i) => {
 
 // render restaurant markers
 function renderRestaurantMarkers() {
-  debugger
   state.restaurants.forEach( marker => {
     const markerEl = document.createElement('div');
     markerEl.dataset.id = marker.id;
@@ -409,34 +547,154 @@ function renderRestaurantMarkers() {
       .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
         .setHTML(`
           <h6>${marker.name}</h6>
+          <p>${marker.priceLevel ? `Price Level: ${marker.priceLevel}<br>` : ''}
+          Rating: ${marker.rating}<br>
+          Total Reviews: ${marker.totalReviews}
+          </p>
+          <button class='btn btn-light add-rest-btn'>Add to Night</button>
         `))
       .addTo(map);
+    state.restaurantMarkers.push(mapMarker);
+    markerEl.addEventListener('click', event => {
+      restId = parseInt(event.target.dataset.id)
+      state.restaurantSelected = state.restaurants.filter(r => r.id === restId)[0];
+      state.selectedRestaurantMarker = state.restaurantMarkers.filter(marker => restId == marker._element.dataset.id)[0];
+    })
   })
+}
+
+// add event listener to document for when bars and restaurants are added to sidebar schedule
+function eventListenerForAddRestToSchedule() {
+  document.addEventListener('click', event => {
+    if(event.target.className.includes('add-rest-btn')) {
+      state.restaurantAdded = true;
+      addRestToSchedule();
+      removeUnselectedRestaurantMarkers();
+    } else if(event.target.className.includes('add-bar-btn')) {
+      state.barAdded = true;
+      addBarToSchedule();
+      removeUnselectedBarMarkers();
+    }
+  })
+}
+
+// remove unselected restaurant markers
+function removeUnselectedRestaurantMarkers() {
+  const unselectedMarkers = [];
+  state.restaurantMarkers.forEach( marker => {
+    if(marker !== state.selectedRestaurantMarker){
+      unselectedMarkers.push(marker)
+    }
+  })
+  unselectedMarkers.forEach(marker => {
+    marker.remove();
+  })
+}
+
+// add restaurant to sidebar Schedule
+function addRestToSchedule() {
+  const schedule = sidebar.querySelector('.side-schedule');
+  const r = state.restaurantSelected;
+  const rInfo = document.createElement('div');
+  rInfo.innerHTML = ''
+  rInfo.className = 'today-events';
+  rInfo.innerHTML = `
+  	<h6>${r.name}</h6>
+    <hr style='width: 90%;'>
+  `
+  schedule.append(rInfo);
+  document.querySelector('.rest-btn').remove();
 }
 
 // ---------------------------------- bars ----------------------------------------
 
 const getNearbyBars = () => {
-  const service = new google.maps.places.PlacesService(document.createElement('div'))
-  const markerLatLng = new google.maps.LatLng({lat: state.selectedEvent.location[1], lng: state.selectedEvent.location[0]})
-  const request = {
-    location: markerLatLng,
-    radius: '500',
-    type: ['bar']
+  if(!state.barAdded) {
+    const service = new google.maps.places.PlacesService(document.createElement('div'))
+    const markerLatLng = new google.maps.LatLng({lat: state.selectedEvent.location[1], lng: state.selectedEvent.location[0]})
+    const request = {
+      location: markerLatLng,
+      radius: '500',
+      type: ['bar']
+    }
+    service.nearbySearch(request, (bars) => {
+      bars.forEach((bar, i) => renderBar(bar, i));
+      renderBarMarkers();
+    });
   }
-  service.nearbySearch(request, (bars) => bars.forEach(renderBar));
 }
 
-const renderBar = bar => {
-  const barName = bar.name
-  const barCoordinates = [bar.geometry.location.lat(), bar.geometry.location.lng()]
-  const barRating = bar.rating
-  const barPriceLevel = bar.price_level
+const renderBar = (bar, i) => {
+  const barName = bar.name;
+  const barCoordinates = [bar.geometry.location.lng(), bar.geometry.location.lat()];
+  const barRating = bar.rating;
+  const barPriceLevel = bar.price_level;
   const newBar = {
+    id: i,
     name: barName,
     coordinates: barCoordinates,
     rating: barRating,
-    priveLevel: restaurantPriceLevel
-  }
-  state.bars.push(newBar)
+    priveLevel: barPriceLevel
+  };
+  state.bars.push(newBar);
+  ++i;
 }
+
+// render restaurant markers
+function renderBarMarkers() {
+  state.bars.forEach( marker => {
+    const markerEl = document.createElement('div');
+    markerEl.dataset.id = marker.id;
+    markerEl.className = 'bar-marker';
+    const mapMarker = new mapboxgl.Marker(markerEl)
+      .setLngLat(marker.coordinates)
+      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(`
+          <h6>${marker.name}</h6>
+          <p>${marker.priceLevel ? `Price Level: ${marker.priceLevel}<br>` : ''}
+          Rating: ${marker.rating}<br>
+          </p>
+          <button class='btn btn-light add-bar-btn'>Add to Night</button>
+        `))
+      .addTo(map);
+    state.barMarkers.push(mapMarker);
+    markerEl.addEventListener('click', event => {
+      barId = parseInt(event.target.dataset.id)
+      state.barSelected = state.bars.filter(bar => bar.id === barId)[0];
+      state.selectedBarMarker = state.barMarkers.filter(marker => barId == marker._element.dataset.id)[0];
+    })
+  })
+}
+
+// add bar to side bar schedule
+function addBarToSchedule() {
+  const schedule = sidebar.querySelector('.side-schedule');
+  const bar = state.barSelected;
+  const barInfo = document.createElement('div');
+  barInfo.innerHTML = ''
+  barInfo.className = 'today-events';
+  barInfo.innerHTML = `
+  	<h6>${bar.name}</h6>
+
+    <hr style='width: 90%;'>
+  `
+  schedule.append(barInfo);
+  document.querySelector('.bar-btn').remove();
+}
+
+// remove unselected bar markers
+function removeUnselectedBarMarkers() {
+  const unselectedMarkers = [];
+  state.barMarkers.forEach( marker => {
+    if(marker !== state.selectedBarMarker){
+      unselectedMarkers.push(marker)
+    }
+  })
+  unselectedMarkers.forEach(marker => {
+    marker.remove();
+  })
+}
+
+// -------------------- on page load -----------------------
+
+signingInEventListener();
